bfbb2f6fa02347f6db3de1bb11c09161ed7b35e1
minjun <ijmtylhtf@naver.com>
2023-11-18 14:48:23 +0900

    Factory Pattern Before Refactoring

---
+++ src/Factory/BeforeRefactoring/Client.java
@@ -0,0 +1,15 @@
+package Factory.BeforeRefactoring;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        Ship whiteship = ShipFactory.orderShip("whiteship");
+        System.out.println("whiteship's color: = " + whiteship.getColor());
+
+        Ship blackShip = ShipFactory.orderShip("blackship");
+        System.out.println("blackShip's color: = " + blackShip.getColor());
+    }
+
+}
---
+++ src/Factory/BeforeRefactoring/ShipFactory.java
@@ -0,0 +1,31 @@
+package Factory.BeforeRefactoring;
+
+import Factory.BeforeRefactoring.Ship;
+
+public class ShipFactory {
+    public static Ship orderShip(String name){
+        validate(name);
+        prepareFor(name);
+
+        Ship ship = new Ship();
+        ship.setName(name);
+
+        if(name.equalsIgnoreCase("whiteship"))
+            ship.setColor("white");
+        else if (name.equalsIgnoreCase("blackship")) {
+            ship.setColor("black");
+        }
+
+        return ship;
+    }
+    private static void validate(String name){
+        if(name == null || name.isBlank()){
+            throw new RuntimeException("Invalid name");
+        }
+    }
+
+    private static void prepareFor(String name){
+        System.out.println("Preparing for building = " + name);
+    }
+
+}
