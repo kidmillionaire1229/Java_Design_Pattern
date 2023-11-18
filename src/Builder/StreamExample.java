565f6e28b81413b43a93fee0c519a975e748cedd
minjun <ijmtylhtf@naver.com>
2023-11-18 16:18:01 +0900

    java.util.Stream example

---
+++ src/Builder/StreamExample.java
@@ -0,0 +1,15 @@
+package Builder;
+
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        Stream.Builder<String> stringStreamBuilder = Stream.builder();
+        Stream<String> names = stringStreamBuilder
+                .add("keesun")
+                .add("whiteship")
+                .build();
+        names.forEach(System.out::println);
+    }
+
+}
