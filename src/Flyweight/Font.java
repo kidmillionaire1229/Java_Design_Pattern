cd24a55b5fa48c0041dee7a2dc6d32b9da06106a
minjun <ijmtylhtf@naver.com>
2023-11-18 19:56:21 +0900

    intrinsic object(flyweight)

---
+++ src/Flyweight/Font.java
@@ -0,0 +1,19 @@
+package Flyweight;
+
+public class Font {
+
+    private final String family;
+    private int size;
+    public Font(String family, int size) {
+        this.family = family;
+        this.size = size;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
