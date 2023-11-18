b39229fa323a9ca10c8b228fa2ede9bd74ca7a32
minjun <ijmtylhtf@naver.com>
2023-11-18 16:14:51 +0900

    java.lang.StringBuilder Example

---
+++ src/Builder/StringBuilderExample.java
@@ -0,0 +1,14 @@
+package Builder;
+
+public class StringBuilderExample {
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String result = stringBuilder
+                .append("whiteship")
+                .append("keesun")
+                .toString();
+        System.out.println("result = " + result);
+    }
+
+}
