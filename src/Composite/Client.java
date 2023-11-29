aa079165e818086e6210c7cc26a0069465718eed
minjun <ijmtylhtf@naver.com>
2023-11-29 20:08:16 +0900

    출력 코드 수정

--- src/Composite/Client.java
+++ src/Composite/Client.java
@@ -2,8 +2,6 @@ package Composite;
 
 public class Client {
     public void printPrice(Component box){
-        int totalPrice = box.getPrice();
-        String line = String.format("Total price of %s: %d", box.getName(), box.getPrice());
-        System.out.println("line = " + line);
+        System.out.printf("Total price of %s: %d", box.getName(), box.getPrice());
     }
 }
