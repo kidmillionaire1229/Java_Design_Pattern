7c73f0826f39902190e256a403214a1312bc0ad8
minjun <ijmtylhtf@naver.com>
2023-12-07 21:09:52 +0900

    Strategy Pattern Example :Comparator in java

---
+++ src/Behavior/Strategy/example/StrategyInJava.java
@@ -0,0 +1,27 @@
+package Behavior.Strategy.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class StrategyInJava {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(1);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(4);
+
+        Collections.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        System.out.println(numbers);
+    }
+
+}
